365e1237e020e35c85d89b899744d483be3c3edd
Gabriel Silveira Marques <g.smarques.57@gmail.com>
2025-09-19 14:15:33 -0300

    Adjust the migration to create Database on the root, and adjust the controler to return the created ID

--- src/server/controllers/cities/Create.ts
+++ src/server/controllers/cities/Create.ts
@@ -33,9 +33,9 @@ export const createCity = async (req: Request<{},{},ICity>, res: Response) => {
     }
 
     const data = req.body;
-    console.log(`Creating city with data: ${data.name}`);
-    return res.status(201).json(
-        { message: `${data.name} created successfully` });
+    console.log(`Creating city with name: ${data.name}`);
+    return res.status(StatusCodes.CREATED).json({ id: result });
+
     // Logic to create a new city would go here 
 }
 
