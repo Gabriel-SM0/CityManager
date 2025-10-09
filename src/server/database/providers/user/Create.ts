d00f10cc16184f6072dbcb26cbd3ef09c9d5d89a
Gabriel Silveira Marques <g.smarques.57@gmail.com>
2025-10-09 13:53:26 -0300

    Added providers for user

---
+++ src/server/database/providers/user/Create.ts
@@ -0,0 +1,52 @@
+
+import { IPerson, IUser } from "../../models";
+import { ETableNames } from "../../ETableNames";
+import { KnexConection } from "../../index";
+
+
+export const create = async (user: Omit<IUser, 'id'>): Promise<number | Error> => {
+    try {
+        const [result] = await KnexConection(ETableNames.user).insert(user).returning('id');
+
+        console.log(`${JSON.stringify(result)} inserted on the database`)
+
+        if (typeof result === 'object') {
+            return result.id;
+        } else if (typeof result === 'number') {
+            return result;  
+        }
+
+        return new Error('Error trying to add one user on the data base')
+
+
+    } catch (error: any) {
+        return new Error(error.message)
+    }
+}
+
+const createFunctionTest = async (user: Omit<IUser, 'id'>): Promise<number | Error> => {
+    try {
+        const [result] = await KnexConection(ETableNames.user).insert(user).returning('id');
+        /*
+        [
+        { id: 42},
+        {id: 55}
+        ]
+
+
+        */
+
+        if (typeof result === 'object') {
+            return result.id;
+        }
+
+        return new Error('Error trying to add one user.')
+
+
+    } catch (error: any) {
+        return new Error(error.message)
+    }
+
+}
+
+
